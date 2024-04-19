34918b95e653ba21e62338ffa694336c338f1093
takumines <takumines1@gmail.com>
2024-04-19 09:02:44 +0900

    tailwindcssの設定変更

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,16 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./_components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [],
   theme: {
     colors: {
       black: "#01233B",
-      "blue-gray": "#CEDAE2",
-      "sky-blue": "#65A5C9",
+      blueGray: "#CEDAE2",
+      skyBlue: "#65A5C9",
+      white: "#FFFFFF",
     },
+    extend: {},
   },
 }
 export default config
