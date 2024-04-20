fed660867bd3da81a5ed13adecba50c50964a67a
takumines <takumines1@gmail.com>
2024-04-20 15:49:00 +0900

    記事一覧ページのAPI繋ぎ込み

--- app/(routes)/(top)/_components/article-summary-card.tsx
+++ app/(routes)/(top)/_components/article-summary-card.tsx
@@ -1,9 +1,12 @@
 import TagIcon from "@/app/_components/elements/icon/tag-icon"
 import UpdatedAtIcon from "@/app/_components/elements/icon/updated-at-icon"
+import { Article } from "@/app/_features/article/types/type"
 import Image from "next/image"
 import Link from "next/link"
 
-const ArticleSummaryCard = () => {
+const ArticleSummaryCard = ({ article }: { article: Article }) => {
+  const { date, slug, tags, title } = article
+
   return (
     <div className="hover:scal-y-[1.02] rounded-xl bg-skyBlue drop-shadow-[0_0px_10px_rgba(0,0,0,0.6)] duration-200 hover:translate-y-[-5px]">
       <Image
@@ -14,26 +17,28 @@ const ArticleSummaryCard = () => {
         width={406}
       />
       <div className="bg-gray-100 p-6 text-black">
-        <Link href="/articles/3">
+        <Link href={`/articles/${slug}`}>
           <h2 className="justify-center break-all text-[24px] font-bold">
-            これはサンプル記事タイトルです
+            {title}
           </h2>
         </Link>
-        <div className="mt-2">
-          <div className="flex items-center">
-            <TagIcon
-              className="text-[#ADFF2F]/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]"
-              height={16}
-              width={16}
-            />
-            <p className="ml-1 text-[16px] text-[#ADFF2F]/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]">
-              Development
-            </p>
-          </div>
+        <div className="mt-2 flex">
+          {tags.map((tag) => (
+            <div className="mr-3 flex items-center" key={tag}>
+              <TagIcon
+                className="text-[#ADFF2F]/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]"
+                height={16}
+                width={16}
+              />
+              <p className="ml-1 text-[16px] text-[#ADFF2F]/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]">
+                {tag}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="flex items-center justify-end">
           <UpdatedAtIcon className="text-black/[.7]" height={20} width={20} />
-          <p className="ml-1 text-[16px] text-black/[.7]">2024-04-01</p>
+          <p className="ml-1 text-[16px] text-black/[.7]">{date}</p>
         </div>
       </div>
     </div>
