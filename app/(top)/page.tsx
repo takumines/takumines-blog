b783f96815defacc095203b2a6b5dd89d5a88d9a
takumines <takumines1@gmail.com>
2024-04-20 13:45:59 +0900

    TOPページコンポーネントをリファクタ

--- app/(top)/page.tsx
+++ app/(top)/page.tsx
@@ -1,118 +1,19 @@
-import Link from "next/link"
+import ArticleSummaryCard from "@/app/(top)/_components/article-summary-card"
+
+const ArticleList = () => {
+  return (
+    <div className="grid grid-cols-2 gap-9">
+      {[...Array(4)].map((_, i) => (
+        <ArticleSummaryCard key={i} />
+      ))}
+    </div>
+  )
+}
 
 const Top = () => {
   return (
     <div className="mt-[56px]">
-      <div className="grid grid-cols-2 gap-9">
-        <Link
-          className="hover:scal-y-[1.02] rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)] hover:translate-y-[-5px]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-        <Link
-          className="rounded-xl bg-skyBlue p-6 drop-shadow-[0_0px_10px_rgba(0,0,0,0.4)]"
-          href="/articles/3"
-        >
-          <div className="bg-gray-100 text-white">
-            <h2 className="text-[24px]">blog title</h2>
-          </div>
-          <p className="text-white">2024-04-01</p>
-        </Link>
-      </div>
+      <ArticleList />
     </div>
   )
 }
