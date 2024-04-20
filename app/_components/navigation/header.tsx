848c042824d25d915b206f25ff3861883b4109b3
takumines <takumines1@gmail.com>
2024-04-20 14:17:01 +0900

    ヘッダーにロゴ画像を表示

--- app/_components/navigation/header.tsx
+++ app/_components/navigation/header.tsx
@@ -1,15 +1,25 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 bg-blueGray">
+    <header className="sticky top-0 z-10 bg-blueGray/[.95]">
       <div className="mx-auto max-w-5xl px-6">
         <div className="h-14 leading-[54px]">
           <Link
-            className="text-[24px] text-black drop-shadow-[0_0px_10px_rgba(0,0,0,0.9)]"
+            className="text-[24px] text-black drop-shadow-[0_0px_8px_rgba(0,0,0,0.9)]"
             href="/"
           >
-            takumines blog
+            <div className="flex items-center">
+              <Image
+                alt="takumines blog logo"
+                className="mr-2 rounded-full"
+                height={28}
+                src="/images/logo.png"
+                width={28}
+              />
+              takumines blog
+            </div>
           </Link>
         </div>
       </div>
