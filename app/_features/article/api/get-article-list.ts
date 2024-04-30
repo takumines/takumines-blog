3b8c62d186a95025b3e314c7a9e46a7c57f1e65d
takumines <takumines1@gmail.com>
2024-04-30 19:38:42 +0900

    [update] 記事一覧取得処理を修正

--- app/_features/article/api/get-article-list.ts
+++ app/_features/article/api/get-article-list.ts
@@ -1,30 +1,14 @@
-import {
-  isMultiSelect,
-  isPageObjectResponse,
-  isRichText,
-  isTitle,
-} from "@/app/_features/article/types"
-import { Article, MultiSelectType } from "@/app/_features/article/types/type"
-import notionClient from "@/app/_lib/notion"
-import { formatISODateTimeToDate } from "@/app/_utils/date"
-import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints"
-
-/**
- * タグの取得
- *
- * @param {MultiSelectType} tags
- * @returns {string[]}
- */
-const getTags = (tags: MultiSelectType): string[] => {
-  return tags.map((tag) => tag.name)
-}
+import { getPageMetaData } from "@/app/_features/article/api/get-page-metadata"
+import { isPageObjectResponse } from "@/app/_features/article/types"
+import { Article } from "@/app/_features/article/types/type"
+import { notionClient } from "@/app/_lib/notion"
 
 /**
  * 記事一覧を取得
  *
  * @returns {Promise<Article[]>}
  */
-const getArticleList = async (): Promise<Article[]> => {
+export const getArticleList = async (): Promise<Article[]> => {
   const res = await notionClient.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
   })
@@ -44,38 +28,3 @@ const getArticleList = async (): Promise<Article[]> => {
     })
     .filter((article): article is Article => !!article)
 }
-
-/**
- * 記事のメタデータを取得
- *
- * @param {PageObjectResponse} post
- * @returns {Post}
- */
-const getPageMetaData = (post: PageObjectResponse): Article => {
-  const { created_time, id, properties } = post
-
-  const date = formatISODateTimeToDate(created_time)
-  const description = isRichText(properties.Description)
-    ? properties.Description.rich_text[0].plain_text
-    : ""
-  const slug = isRichText(properties.Slug)
-    ? properties.Slug.rich_text[0].plain_text
-    : ""
-  const title = isTitle(properties.Name)
-    ? properties.Name.title[0].plain_text
-    : ""
-  const tags = isMultiSelect(properties.Tags)
-    ? getTags(properties.Tags.multi_select)
-    : []
-
-  return {
-    date,
-    description,
-    id,
-    slug,
-    tags,
-    title,
-  }
-}
-
-export default getArticleList
