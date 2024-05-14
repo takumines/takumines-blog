697ba27cc27d7b33ec5e3c50e4038e918bd8311a
takumines <takumines1@gmail.com>
2024-05-15 00:06:58 +0900

    [update] Card全体がリンクになるよう変更

--- app/(routes)/(top)/_components/article-summary-card.tsx
+++ app/(routes)/(top)/_components/article-summary-card.tsx
@@ -8,44 +8,44 @@ const ArticleSummaryCard = ({ article }: { article: Article }) => {
   const { date, slug, tags, title } = article
 
   return (
-    <div className="hover:scal-y-[1.02] rounded-xl bg-skyBlue drop-shadow-[0_0px_10px_rgba(0,0,0,0.6)] duration-200 hover:translate-y-[-5px]">
-      <Image
-        alt="サムネイル画像"
-        className="rounded-t-xl"
-        height={232}
-        src={article.thumbnail}
-        style={{
-          height: "auto",
-          width: "100%",
-        }}
-        width={406}
-      />
-      <div className="p-6 text-black">
-        <Link href={`/articles/${slug}`}>
+    <Link href={`/articles/${slug}`}>
+      <div className="hover:scal-y-[1.02] rounded-xl bg-skyBlue drop-shadow-[0_0px_10px_rgba(0,0,0,0.6)] duration-200 hover:translate-y-[-5px]">
+        <Image
+          alt="サムネイル画像"
+          className="rounded-t-xl"
+          height={232}
+          src={article.thumbnail}
+          style={{
+            height: "auto",
+            width: "100%",
+          }}
+          width={406}
+        />
+        <div className="p-6 text-black">
           <h2 className="justify-center break-all text-[24px] font-bold">
             {title}
           </h2>
-        </Link>
-        <div className="mt-2 flex">
-          {tags.map((tag) => (
-            <div className="mr-3 flex items-center" key={tag}>
-              <TagIcon
-                className="text-limeGreen/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]"
-                height={16}
-                width={16}
-              />
-              <p className="ml-1 text-[16px] text-limeGreen/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]">
-                {tag}
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="flex items-center justify-end">
-          <UpdatedAtIcon className="text-black/[.7]" height={20} width={20} />
-          <p className="ml-1 text-[16px] text-black/[.7]">{date}</p>
+          <div className="mt-2 flex">
+            {tags.map((tag) => (
+              <div className="mr-3 flex items-center" key={tag}>
+                <TagIcon
+                  className="text-limeGreen/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]"
+                  height={16}
+                  width={16}
+                />
+                <p className="ml-1 text-[16px] text-limeGreen/[.6] drop-shadow-[0_0px_4px_rgba(0,0,0,0.5)]">
+                  {tag}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center justify-end">
+            <UpdatedAtIcon className="text-black/[.7]" height={20} width={20} />
+            <p className="ml-1 text-[16px] text-black/[.7]">{date}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
