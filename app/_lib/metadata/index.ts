41b2d7251997fa6eda5b57cb5c6e2ce3fb050e06
takumines <takumines1@gmail.com>
2024-05-14 23:45:08 +0900

    [add] メタデータのベースとなる定数を管理するファイルを作成

---
+++ app/_lib/metadata/index.ts
@@ -0,0 +1,15 @@
+const META_TITLE = "takumines blog"
+const META_DESCRIPTION = "Personal blog by takumines"
+export const BASE_METADATA = {
+  description: META_DESCRIPTION,
+  openGraph: {
+    images: "/images/ogp.png",
+    type: "website",
+  },
+  title: META_TITLE,
+  twitter: {
+    description: META_DESCRIPTION,
+    images: "/images/ogp.png",
+    title: META_TITLE,
+  },
+}
