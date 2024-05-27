3ef8f914c461ae8299c6547fd132c164707b7248
takumines <takumines1@gmail.com>
2024-05-27 22:47:07 +0900

    [update] 記事詳細APIのリクエストをメモ化するよう修正

--- app/_features/article/api/get-article-detail.ts
+++ app/_features/article/api/get-article-detail.ts
@@ -5,49 +5,50 @@ import {
   isPageObjectResponse,
 } from "@/app/_features/article/types"
 import { n2m, notionClient } from "@/app/_lib/notion"
+import { cache } from "react"
 
-export const getArticleDetail = async (
-  id: string,
-): Promise<ArticleDetail | NotionApiError | undefined> => {
-  try {
-    const res = await notionClient.databases.query({
-      database_id: process.env.NOTION_DATABASE_ID!,
-      filter: {
-        and: [
-          {
-            formula: {
-              string: {
-                equals: id,
+export const getArticleDetail = cache(
+  async (id: string): Promise<ArticleDetail | NotionApiError | undefined> => {
+    try {
+      const res = await notionClient.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID!,
+        filter: {
+          and: [
+            {
+              formula: {
+                string: {
+                  equals: id,
+                },
+              },
+              property: "Slug",
+            },
+            {
+              property: "Status",
+              select: {
+                equals: "Publish",
               },
             },
-            property: "Slug",
-          },
-          {
-            property: "Status",
-            select: {
-              equals: "Publish",
-            },
-          },
-        ],
-      },
-    })
+          ],
+        },
+      })
 
-    const pageObject =
-      res.results.length > 0 && isPageObjectResponse(res.results[0])
-        ? res.results[0]
-        : undefined
+      const pageObject =
+        res.results.length > 0 && isPageObjectResponse(res.results[0])
+          ? res.results[0]
+          : undefined
 
-    if (!pageObject) {
-      return undefined
+      if (!pageObject) {
+        return undefined
+      }
+
+      const content = await n2m(pageObject.id)
+
+      return {
+        ...getPageMetaData(pageObject),
+        content,
+      }
+    } catch (error) {
+      return new NotionApiError(error)
     }
-
-    const content = await n2m(pageObject.id)
-
-    return {
-      ...getPageMetaData(pageObject),
-      content,
-    }
-  } catch (error) {
-    return new NotionApiError(error)
-  }
-}
+  },
+)
