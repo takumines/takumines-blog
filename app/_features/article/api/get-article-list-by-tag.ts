54d43595aa31fe79e131f4d3ca7112d285c502d2
takumines <takumines1@gmail.com>
2024-09-14 11:34:50 +0900

    [add] タグに紐づく記事一覧取得する関数を作成

---
+++ app/_features/article/api/get-article-list-by-tag.ts
@@ -0,0 +1,49 @@
+import { getPageMetaData } from "@/app/_features/article/api/get-page-metadata"
+import { NotionApiError } from "@/app/_features/article/error"
+import { Article, isPageObjectResponse } from "@/app/_features/article/types"
+import { notionClient } from "@/app/_libs/notion"
+
+export const getArticleListByTag = async (
+  tag: string,
+): Promise<Article[] | NotionApiError> => {
+  try {
+    const res = await notionClient.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        and: [
+          {
+            multi_select: {
+              contains: tag,
+            },
+            property: "Tags",
+          },
+          {
+            property: "Status",
+            select: {
+              equals: "Publish",
+            },
+          },
+        ],
+      },
+    })
+    console.log(res)
+
+    const articleList = res.results
+
+    if (!articleList) {
+      return []
+    }
+
+    return articleList
+      .map((article) => {
+        const pageObject = isPageObjectResponse(article) ? article : undefined
+        if (!pageObject) {
+          return pageObject
+        }
+        return getPageMetaData(pageObject)
+      })
+      .filter((article): article is Article => !!article)
+  } catch (error) {
+    return new NotionApiError(error)
+  }
+}
--- app/_features/article/api/index.ts
+++ app/_features/article/api/index.ts
@@ -1,3 +1,4 @@
 export * from "@/app/_features/article/api/get-article-detail"
 export * from "@/app/_features/article/api/get-article-list"
+export * from "@/app/_features/article/api/get-article-list-by-tag"
 export * from "@/app/_features/article/api/get-tag-list"
