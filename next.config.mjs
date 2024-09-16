bb50e138362ea9966ce539a9229d55fd2cd5b012
takumines <takumines1@gmail.com>
2024-09-16 15:21:36 +0900

    [fix] 利用可能な画像のドメインを修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ['tf0rxl2lxkwyfsdw.public.blob.vercel-storage.com'],
-	}
+  images: {
+    domains: [
+      "tf0rxl2lxkwyfsdw.public.blob.vercel-storage.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
+    ],
+  },
 }
 
 export default nextConfig
